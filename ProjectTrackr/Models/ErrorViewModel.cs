81be7dfd40fa111d9f26b0e23a33fd608c8d52e9
Youssef <youss.eljaddaoui@gmail.com>
2025-12-06 16:54:29 +0100

    Created MVC project

---
+++ ProjectTrackr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectTrackr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
